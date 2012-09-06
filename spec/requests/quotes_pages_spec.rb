0dab02b23e04b345cc2c1e317023e972785a2fff
Angel Villanueva <avillanueva@sinapse.es>
2012-09-06 13:11:09 +0200

    Added Massive Assignment test for Quotes

--- spec/requests/quotes_pages_spec.rb
+++ spec/requests/quotes_pages_spec.rb
@@ -11,6 +11,24 @@ describe "Quotes" do
     it { should respond_to :number_cylinders }
     it { should respond_to :stroke }
     it { should respond_to :plaking_date }
+    
+    describe "accessible attributes" do
+      it "should now allow price massive assignment" do
+        expect do
+          Quote.new(price: 100)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+      it "should now allow vehicle massive assignment" do
+        expect do
+          Quote.new(vehicle_id: 1)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+      it "should now allow city massive assignment" do
+        expect do
+          Quote.new(city_id: 1)
+        end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+      end
+    end
   end
 
 end
