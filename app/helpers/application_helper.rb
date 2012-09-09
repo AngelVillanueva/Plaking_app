bcd50b99633fa70b7b8ecc31675536d1713a8564
Angel Villanueva <avillanueva@sinapse.es>
2012-09-09 16:38:28 +0200

    Helper for Full_title added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+ # Returns the full title on a per-page basis.       # Documentation comment
+   def full_title(page_title)                          # Method definition
+     base_title = "Matricula el coche"  # Variable assignment
+     if page_title.empty?                              # Boolean test
+       base_title                                      # Implicit return
+     else
+       "#{page_title} | #{base_title}"                 # String interpolation
+     end
+   end
+   
 end
