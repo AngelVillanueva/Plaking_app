f2fe7c50a5bf9fec4eaf4b72ca2bfee9075b239f
Angel Villanueva <avillanueva@sinapse.es>
2012-10-24 22:46:30 +0200

    Extended helper to highlight also Admin active page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,12 +11,14 @@ module ApplicationHelper
    end
 
  # Returns current_page?
-   def class_if_link(this_page)
+   def class_if_link(this_page, title ="")
    	 case this_page
    	 	when 'home'
    	 		" class=active" if current_page? '/'
    	 	when 'account'
    	 		" class=active" if current_page? "/users/#{current_user.id}"
+   	 	when 'admin'
+   	 		" active" if title.include?("Admin")
    	 	end
    end
    
