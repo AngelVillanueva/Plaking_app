f3395308456c29968f975e3cfbc0ba02f10a072f
Angel Villanueva <avillanueva@sinapse.es>
2012-10-25 16:32:59 +0200

    Minor images updates

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module LlorensApp
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '2.0'
+    config.assets.version = '3.0'
   end
 end
